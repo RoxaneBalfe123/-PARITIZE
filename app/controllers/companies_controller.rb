e3f28eff868b2fca1454b55bc40fdca2ad65a239
Roxanne Balfe <90247916+RoxaneBalfe123@users.noreply.github.com>
2022-02-22 19:37:56 +0000

    companies index: added route, controller, and view for basic index

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,2 +1,12 @@
 class CompaniesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
+
+  def index
+    @companies = Company.all
+    # if params[:query].present?
+    #@company = Company.search_by_name(params[:query])and search by category
+    #else
+    # @companies = Company.all
+    #end
+  end
 end
