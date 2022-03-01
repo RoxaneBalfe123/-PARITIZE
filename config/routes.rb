45a4af3d4647bbec4764e7921eb90009f025c78d
Lucia Moral <lmoralbaena@gmail.com>
2022-03-01 19:58:40 +0100

    feat: complete about us page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-
+  get 'about', to: 'pages#about'
   resources :companies
   resources :gender_ratings, only: [:show]
 
