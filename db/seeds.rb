ba82e00538d8b8e6e2ef78ee8aa69e8ba1fdbe26
Lucia Moral <lmoralbaena@gmail.com>
2022-03-01 22:17:04 +0100

    feat: complete show and seeded again

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,11 @@
 
 require "open-uri"
 puts "clean DB"
-User.destroy_all
-Company.destroy_all
 Bookmark.destroy_all
+GenderRating.destroy_all
+Company.destroy_all
+User.destroy_all
+
 
 puts "lets create some companies"
 
@@ -56,6 +58,16 @@ astra_zeneca.photo.attach(io: file, filename: 'astrazeneca_jxzwhh.png', content_
 astra_zeneca.user_id = user.id
 astra_zeneca.save!
 
+gender_rating = GenderRating.new(
+  c_rating: 12.3,
+  mid_rating: 23.0,
+  junior_rating: 21.1,
+  average_rating: 40,
+  company_id: astra_zeneca.id
+)
+gender_rating.company_id = astra_zeneca.id
+gender_rating.save!
+
 file = URI.open('https://res.cloudinary.com/dqpmvek08/image/upload/v1645731862/gsk_q6awrt.png')
   gsk = Company.new(
     company_name: "GSK",
@@ -70,7 +82,8 @@ file = URI.open('https://res.cloudinary.com/dqpmvek08/image/upload/v1645731862/g
     hq_location: location.sample,
     company_description: "GlaxoSmithKline PLC is a global healthcare company.
     The Company researches and develops a range of products in three primary
-    areas: Pharmaceuticals, Vaccines and Consumer Healthcare."
+    areas: Pharmaceuticals, Vaccines and Consumer Healthcare.",
+
   )
 
 gsk.photo.attach(io: file, filename: 'gsk_q6awrt.png', content_type: 'image/png')
@@ -92,7 +105,8 @@ file = URI.open('https://res.cloudinary.com/dqpmvek08/image/upload/v1645731862/b
     company_description: "Bayer is a Life Science company with a more than 150-year
     history and core competencies in the areas of health care and agriculture. With
     our innovative products, we are contributing to finding solutions to some of the
-    major challenges of our time."
+    major challenges of our time.",
+
   )
 
 bayer.photo.attach(io: file, filename: 'bayer_qudgqa', content_type: 'image/png')
@@ -114,7 +128,8 @@ file = URI.open('https://res.cloudinary.com/dqpmvek08/image/upload/v1645731862/m
     company_description: "Merck & Co. manufactures a variety of well-known
     pharmaceutical drugs, vaccines and animal-health products. It made the first
     smallpox vaccine for commercial use in the U.S. It is also the maker of the
-    painkiller Vioxx and the HPV vaccine Gardasil."
+    painkiller Vioxx and the HPV vaccine Gardasil.",
+
   )
 
 merck.photo.attach(io: file, filename: 'merck_tbwmyp.png', content_type: 'image/png')
@@ -136,7 +151,8 @@ file = URI.open('https://res.cloudinary.com/dqpmvek08/image/upload/v1645731863/r
     company_description: "Headquartered in Basel, Switzerland, Roche is a leader
     in research-focused healthcare with combined strengths in pharmaceuticals and
     diagnostics. Roche is the world's largest biotech company, with truly differentiated
-    medicines in oncology, immunology, infectious diseases, ophthalmology and neuroscience."
+    medicines in oncology, immunology, infectious diseases, ophthalmology and neuroscience.",
+
   )
 
 roche.photo.attach(io: file, filename: 'roche_xnrlgh.png', content_type: 'image/png')
@@ -158,7 +174,8 @@ file = URI.open('https://res.cloudinary.com/dqpmvek08/image/upload/v1645731862/f
     company_description: "Facebook is a website which allows users, who sign-up for free profiles,
     to connect with friends, work colleagues or people they don't know, online.
     It allows users to share pictures, music, videos, and articles, as well as
-    their own thoughts and opinions with however many people they like."
+    their own thoughts and opinions with however many people they like.",
+
   )
 
 facebook.photo.attach(io: file, filename: 'facebook_nyiok4.png', content_type: 'image/png')
@@ -180,7 +197,8 @@ file = URI.open('https://res.cloudinary.com/dqpmvek08/image/upload/v1645731862/g
     company_description: "Google is an American company that is most commonly
      known as a search engine. Although the company made it's name as a search engine,
      and the vast majority of its income comes from advertising because of this,
-     it has branched out into a number of areas such as cloud computing, software and hardware."
+     it has branched out into a number of areas such as cloud computing, software and hardware.",
+
   )
 
 google.photo.attach(io: file, filename: 'google_rwfzvl.png', content_type: 'image/png')
@@ -202,7 +220,8 @@ file = URI.open('https://res.cloudinary.com/dqpmvek08/image/upload/v1645731862/t
     company_description: "Twitter is known as a micro-blogging site. Blogging has
     been around for some time. Usually blogging consists of people setting up basic
     websites where they write about whatever they want, whether it be politics, sport,
-    cooking, fashion etc."
+    cooking, fashion etc.",
+
   )
 
 twitter.photo.attach(io: file, filename: 'twitter_qb63cp.png', content_type: 'image/png')
@@ -224,7 +243,8 @@ file = URI.open('https://res.cloudinary.com/dqpmvek08/image/upload/v1645731863/s
     company_description: "SpaceX was formed by entrepreneur Elon Musk in the hopes
     of revolutionizing the aerospace industry and making affordable spaceflight a
     reality. The company entered the arena with the Falcon 1 rocket, a two-stage
-    liquid-fueled craft designed to send small satellites into orbit."
+    liquid-fueled craft designed to send small satellites into orbit.",
+
   )
 
 spacex.photo.attach(io: file, filename: 'space_x_hbmydy.png', content_type: 'image/png')
@@ -246,7 +266,8 @@ file = URI.open('https://res.cloudinary.com/dqpmvek08/image/upload/v1645731862/t
     company_description: "Best described as a battery company, Tesla is best known
     for its electric cars. The company is also known for specialising in solar panels
     and Lithium-ion battery energy storage. The company first gained attention following
-    the 2008 release of the Tesla Roadster, the very first electric sports car."
+    the 2008 release of the Tesla Roadster, the very first electric sports car.",
+
   )
 
 tesla.photo.attach(io: file, filename: 'tesla_icwlzv.png', content_type: 'image/png')
@@ -267,7 +288,8 @@ file = URI.open('https://res.cloudinary.com/dqpmvek08/image/upload/v1645731863/s
     hq_location: location.sample,
     company_description: "Siemens Limited is a technology company focused on industry,
     infrastructure, digital transformation, transport as well as transmission and generation
-    of electrical power. It is the flagship listed company of Siemens AG in India."
+    of electrical power. It is the flagship listed company of Siemens AG in India.",
+
   )
 
 siemens.photo.attach(io: file, filename: 'siemens_rleboh.png', content_type: 'image/png')
@@ -289,7 +311,8 @@ file = URI.open('https://res.cloudinary.com/dqpmvek08/image/upload/v1645731862/e
     company_description: "EDF Energy is a British integrated energy company, wholly
     owned by the French state owned EDF (Électricité de France), with operations spanning
     electricity generation and the sale of natural gas and electricity to homes and businesses
-    throughout the United Kingdom."
+    throughout the United Kingdom.",
+
   )
 
 edf_energy.photo.attach(io: file, filename: 'edf_u7rpi8.png', content_type: 'image/png')
@@ -310,7 +333,8 @@ file = URI.open('https://res.cloudinary.com/dqpmvek08/image/upload/v1645731862/b
     hq_location: location.sample,
     company_description: "Boeing is the world's largest aerospace company and leading
     manufacturer of commercial jetliners, defense, space and security systems, and service
-    provider of aftermarket support."
+    provider of aftermarket support.",
+
   )
 
 boeing.photo.attach(io: file, filename: 'boeing_eggbwo.png', content_type: 'image/png')
@@ -332,6 +356,7 @@ file = URI.open('https://res.cloudinary.com/dqpmvek08/image/upload/v1645731863/r
     company_description: "Rolls-Royce is the world's leading supplier of marine
     propulsion equipment. Its products and services range from complete ship design
     to integrated propulsion, maneuvering, and positioning systems and deck machinery."
+
   )
 
 rolls_royce.photo.attach(io: file, filename: 'rolls_royce_i073x2', content_type: 'image/png')
