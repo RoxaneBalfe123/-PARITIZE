04f31418ac961b3a4d9596585f39b2135aa0cd8b
KikoMedina <kotochie@hotmail.com>
2022-03-01 21:29:48 +0100

    Almost done with the navbar

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,13 @@ Rails.application.routes.draw do
   resources :companies do
     resources :bookmarks, only: [:create, :destroy]
   end
-  
+
   get 'about', to: 'pages#about'
-  
+
 
   resources :gender_ratings, only: [:show]
 
-  
+
     get "user/:id", to: "users#show", as: "user_show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
