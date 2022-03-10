d0a45a5dd44a2b2254eb05b933caad2c40cf6157
Lucia Moral <lmoralbaena@gmail.com>
2022-03-10 11:10:25 +0100

    feat: finish frontend for cards

--- app/models/company.rb
+++ app/models/company.rb
@@ -22,9 +22,4 @@ class Company < ApplicationRecord
       tsearch: { prefix: true }
     }
 
-  # pg_search_scope :search_by_company_type,
-  # against: [ :type_of_company ],
-  # using: {
-  # #     tsearch: { prefix: true }
-  #   }
 end
